4081f1052aa89531e8e6f667c59e235915cfc2e2
Michael Martin <mwm.cal@gmail.com>
2015-04-09 00:07:16 -0700

    initial, with jasmine tests

---
+++ ts-web/results.ts
@@ -0,0 +1,47 @@
+﻿"use strict";
+
+class TimeThing {
+    constructor(public hours: number, public minutes: number, public seconds: number) {
+
+    }
+
+    //"00:00:00"
+    //"00:13:24.7960000"
+    static fromString(thing: string) {
+        function getSegments(str: string) {
+            var array = [];
+            var index = 0;
+            while (index < str.length) {
+                var separator = str.indexOf(":", index);
+                var substr: string;
+                if (separator <= 0) {
+                    substr = str.substr(index);
+                    index = str.length;
+                } else {
+                    substr = str.substr(index, separator - index);
+                    index = separator + 1;
+                }
+                array.push(Number(substr));
+            }
+            return array;
+        }
+
+        var segments = getSegments(thing);
+        var hours = segments[0];
+        var minutes = segments[1];
+        var seconds = segments[2];
+
+        return new TimeThing(hours, minutes, seconds);
+    }
+}
+
+class Result {
+    constructor(public bib: number, public checkpoint: number, public time: TimeThing) {
+
+    }
+
+    static fromJson(json: string) {
+        var data = JSON.parse(json);
+        return new Result(data.bib, data.checkpoint, TimeThing.fromString(data.time));
+    }
+}
---
+++ ts-web/tests.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="lib/typings/jasmine/jasmine.d.ts" />
+/// <reference path="results.ts" />
+
+describe("Result tests",() => {
+
+    var expects = JSON.parse("[{\"bib\":1,\"checkpoint\":0,\"time\":\"00:00:00\"},{\"bib\":1,\"checkpoint\":1,\"time\":\"00:26:54.6970000\"}]");
+
+    function iterate (callback) {
+        for (var i = 0; i < expects.length; i++) {
+            var exp = expects[i];
+            callback(exp, Result.fromJson(JSON.stringify(exp)));
+        }
+    }
+    
+    it("has a valid bib",() => {
+        iterate((exp, act) => {
+            expect(act.bib).toBe(exp.bib);
+        });
+    });
+
+    it("has a valid checkpoint",() => {
+        iterate((exp, act) => {
+            expect(act.checkpoint).toBe(exp.checkpoint);
+        });
+    });
+
+    it("has a valid time",() => {
+        iterate((exp, act) => {
+            expect(act.time).toEqual(TimeThing.fromString(exp.time));
+        });
+    });
+});
